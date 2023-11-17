aa8a10939890f2a3b6365d20f0b9dc22b2c65950
Alex <format37@gmail.com>
2023-11-17 14:58:54 +0400

    modified:   README.md 	new file:   requirements.txt 	new file:   ssh.py

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# terminal
-AI agents need to have access to your terminal sometimes
+### Install sshd
+```
+sudo apt install openssh-server
+sudo systemctl start sshd
+```
+### Test sshd
+```
+ssh localhost 2022
+```
+### Install repo
+```
+git clone https://github.com/format37/terminal.git
+cd terminal
+```
+### Test repo
+```
+python3 ssh.py
+```
---
+++ ssh.py
@@ -0,0 +1,47 @@
+import paramiko
+import time
+
+def ssh_session(hostname, port, username, password):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname, port, username, password)
+    channel = client.invoke_shell()
+
+    def send_command(cmd):
+        channel.send(cmd + '\n')
+        time.sleep(1)
+        if channel.recv_ready():
+            return channel.recv(4096).decode('utf-8')
+        return ""
+
+    def send_interrupt():
+        channel.send(chr(0x03))  # Sending Ctrl-C
+        time.sleep(1)
+
+    while True:
+        command = input(
+            "Enter command (type 'exit' to close session, ':' to update, 'stop' to interrupt): "
+            )
+        if command.lower() == 'exit':
+            break
+        elif command.lower() == 'stop':
+            send_interrupt()
+            print("Command interrupted.")
+        else:
+            response = send_command(command)
+            print(response)
+
+    channel.close()
+    client.close()
+
+# Replace with the appropriate hostname, port, username, password, and command
+# input address
+hostname = input("Enter address (localhost): ")
+# input port
+port = input("Enter port (2022): ")
+# input username
+username = input("Enter username: ")
+# input password
+password = input("Enter password: ")
+
+ssh_session(hostname, port, username, password)
